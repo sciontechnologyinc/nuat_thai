44d830e1415670d257d390a80a6c2db0a0d3e50e
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-08-28 03:25:01 +0800

    push

--- resources/views/website/pages/reservation.blade.php
+++ resources/views/website/pages/reservation.blade.php
@@ -1,5 +1,38 @@
 @extends('website.master.template')
 
 @section('content')
+<div class="container reservation-page">
+    <div class="reservation-title">RESERVATION</div>
+    <div class="reservation-desc">Check available schedule for massage and other information</div>
 
+    <table class="table table-sm table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="bg-success">
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr class="bg-danger">
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>  
 @endsection
