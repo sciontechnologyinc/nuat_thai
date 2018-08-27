d88a5b82ccb709a6dd2796532f72709e05bad44b
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-08-28 04:41:53 +0800

    push

--- resources/views/website/pages/aboutus.blade.php
+++ resources/views/website/pages/aboutus.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
 
-<header>
-        <div class="card bg-dark text-white">
-        <img class="card-img" src="/images/aboutus-cover.jpg" alt="Card image">
-        <div class="card-img-overlay">
-          <h5 class="card-title">About Us</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text">Last updated 3 mins ago</p>
+<header class= "aboutheader">
+        <div class="aboutmain">
+        <div class="card about">
+          <h3 class="aboutus1">About Us</h3>
+          <p class="aboutus2"> <b> Thai Services Inc./NUAT THAI Foot and Body Massage </b> originated from Cebu City and started last January 2005. <br> <br> q
+                <b>Nuat Thai Services Inc.,</b> is a corporation created to bring the business name “ NUAT THAI Foot and Body Massage” as well as the franchisor of the said business name. NUAT THAI Foot and Body Massage provides purely authentic Thai massage and Thai aroma therapy. <br> <br>
+                
+                Our services provides relaxation and easing stress to the 20th Century modern workers. This is unique in the sense that the massage is a dry massage applying acupressure to our body, relieving muscle aches and pains, anti-aging, improves blood circulation and relief of stress. These are just some of the benefits of Thai massage.</p>
         </div>      
       </div>
 </header>
