d0426f0330a795b18775c96de8017a4b9365866b
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-08-29 05:57:28 +0800

    push

--- resources/views/website/pages/aboutus.blade.php
+++ resources/views/website/pages/aboutus.blade.php
@@ -3,17 +3,41 @@
 @section('content')
 
 <header class= "aboutheader">
+ <div class="card about1">
         <div class="aboutmain">
-        <div class="card about">
           <h3 class="aboutus1">About Us</h3>
-          <p class="aboutus2"> <b> Thai Services Inc./NUAT THAI Foot and Body Massage </b> originated from Cebu City and started last January 2005. <br> <br> q
+          <p class="aboutus2"> <b> Thai Services Inc./NUAT THAI Foot and Body Massage </b> originated from Cebu City and started last January 2005. <br> <br>
                 <b>Nuat Thai Services Inc.,</b> is a corporation created to bring the business name “ NUAT THAI Foot and Body Massage” as well as the franchisor of the said business name. NUAT THAI Foot and Body Massage provides purely authentic Thai massage and Thai aroma therapy. <br> <br>
                 
                 Our services provides relaxation and easing stress to the 20th Century modern workers. This is unique in the sense that the massage is a dry massage applying acupressure to our body, relieving muscle aches and pains, anti-aging, improves blood circulation and relief of stress. These are just some of the benefits of Thai massage.</p>
-        </div>      
-      </div>
-</header>
+        </div>    
+    
+      <div class="aboutmain">
+       <h3 class="mvs">Mission and Vision</h3>
+        <p class="aboutus2">Is to create and manage an Authentic Thai Massage Center to cater the old and young working class to relieve them of stress and live a healthful life and to address the needs in the field of relaxation in order to cope up a daily challenging environment, Without limiting the generality of the foregoing statement of purpose and it shall include the following;</p>
+      <br> <br> 
 
+      <ul class="mvsul1 offset-4">
+        <Li class="mission1">a. Offer other in demand health massage other than Thai Massage.</Li>
+        <Li class="mission1">b. Promote and be a part of the solution in non-employment</Li>
+        <Li class="mission1">c. Offer franchises to expand and promote its mission and vision</Li>
+        <Li class="mission1">d. To assist entrepreneurs who are into health massage business.</Li>
+        <Li class="mission1"> e. To encourage healthy living thru inexpensive massage services. </Li>
+        <Li class="mission1">f. Continually develop acupressure thru constant training in Thailand </Li>
+        <Li class="mission1"> of their ancient Thai Massage medical school and share it to its franchisees.</Li>
+      </ul>
+       </div>   
+    
+    <div class="aboutmain">
+        <h3 class="loc">Location</h3>
+        <p class="loc2"> <b>NUAT THAI</b> :<i> Katipunan Avenue California Village, Quezon City, 1116 Metro Manila</i> </p>
+        <div class="nt_gmap">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241.18998419844064!2d121.02091388489778!3d14.710344364049392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b11799f5a5bf%3A0xd007e2008613fdea!2sNuat+Thai!5e0!3m2!1sen!2sph!4v1535485215614" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+
+</div> 
+</header>
 
 
 @endsection
