96304c5538476cf4ff37e0608f59a3948aad1c7f
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-08-29 04:40:00 +0800

    push

--- resources/views/website/pages/bookmassage.blade.php
+++ resources/views/website/pages/bookmassage.blade.php
@@ -37,15 +37,16 @@
                     <div class="form-group">
                         <label for="noofreservation">Package</label>
                         <select class="form-control form-control-lg" id="noofreservation">
-                            <option>A1 - 1hr Thai Body Massage + 30 mins Head Massage</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                            <option>7</option>
+                            <option>A1 - 1 hr Thai Body Massage + 30 mins Head Massage</option>
+                            <option>B1 - 1 hr Thai Foot Massage + 30 mins Head Massage</option>
+                            <option>C1 - 1 hr Swedish Massage +  30 mins Head Massage</option>
+                            <option>D1 - 1.5 hr Aromatherapy +  30 mins Head Massage</option>
+                            <option>E1 - 1.5 hr Hot Oil Massage + 30 mins Head Massage</option>
+                            <option>F1 - 1.5 hr Ventosa Massage + 30 mins Head Massage</option>
+                            <option>G1 - 1.5 hr Hot Stone Massage + 30 mins Head Massage</option>
                         </select>
                     </div>
+                    <button type="submit" class="btn btn-success mr-2 bookmassage">Submit</button>
                 </div>
             </div>
         </div>
@@ -58,4 +59,5 @@
         </div>
     </div>
 </div>
+<div class="padding"></div>
 @endsection
