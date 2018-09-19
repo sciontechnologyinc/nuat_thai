17171b7f10de88c7822ca2db71b264653641323e
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-09-20 04:13:05 +0800

    push

--- resources/views/website/pages/services.blade.php
+++ resources/views/website/pages/services.blade.php
@@ -8,17 +8,17 @@
     <div class="services-title">Services/Packages</div>
     <div class="accordion" id="accordionExample">
     @foreach($packages as $package)
-  <div class="card">
-    <div class="card-header" id="headingOne">
+  <div class="card services">
+    <div class="card-header services" id="headingOne">
       <h5 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#{{$package->id}}" aria-expanded="true" aria-controls="{{$package->id}}">
+        <button class="btn btn-link services" type="button" data-toggle="collapse" data-target="#{{$package->id}}" aria-expanded="true" aria-controls="{{$package->id}}">
         {{ $package->packagecode }}
         </button>
       </h5>
     </div>
 
     <div id="{{$package->id }}" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
+      <div class="card-body services">
       {{ $package->packagedescription }}
       </div>
     </div>
@@ -29,7 +29,22 @@
 </div>   
 
 
-</div> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+<div class="container services">
+  <h2></h2>
+   <section class="customer-logos slider">
+      <div class="slide"><img src="/images/slider1.jpg"></div>
+      <div class="slide"><img src="/images/slider2.jpg"></div>
+      <div class="slide"><img src="/images/slider3.jpg"></div>
+      <div class="slide"><img src="/images/slider4.jpg"></div>
+      <div class="slide"><img src="/images/slider5.jpg"></div>
+      <div class="slide"><img src="/images/slider6.jpg"></div>
+      <div class="slide"><img src="/images/slider7.jpg"></div>
+      <div class="slide"><img src="/images/slider8.jpg"></div>
+   </section>
+</div>
+
+<div class="padding"></div>
 </header>
 
 
