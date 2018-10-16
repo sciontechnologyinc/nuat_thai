58251f9dd4fa3487e43692bdf30fdc1004fc54ae
Jetro Macalipay <wajjfreelance@gmail.com>
2018-10-16 20:40:20 +0800

    test
    test

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,46 +2,37 @@
 
 @section('content')
 
+@extends('website.master.template')
 
-@if($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
-@endif
-
- @if(count($errors) > 0 )
-    <div class="alert alert-danger">
-        <strong>Whoooppss !!</strong> There were some problem with your input. <br>
-        <ul>
-          @foreach($errors->all() as $error)
-              <li> {{ $error }} </li>
-          @endforeach
-        </ul>
-    </div>
- @endif
- {!! Form::open(['id' => 'dataForm', 'method' => 'PATCH', 'url' => 'users.edit' . $user->user ]) !!}
- <div class="container reservation-page">
-        <div class="reservation-title mx-0 px-0">Account Settings</div>
-    <div class="card ps">
-        <div class="form-group col-lg-12">
-                {!!Form::label('name', 'Full Name', array('class' => 'form-control-label'))!!}
-                {!!Form::text('name',$user->name, ['placeholder' => '', 'class' => 'form-control col-lg-12', 'required' => '' ])!!}
+@section('content')
+<div class="container reservation-page">
+    <div class="reservation-title mx-0 px-0">Account Settings</div>
+ <form method="post" action="{{route('users.update', $user)}}">
+        {{ csrf_field() }}
+        {{ method_field('patch') }}
+        <div class="form-group">
+            <label for="name">Full Name</label>
+            <input type="text" name="name"  value="{{ $user->name }}" class="form-control col-lg-12"/>
         </div>
-        <div class="form-group col-lg-12">
-                {!!Form::label('email', 'Email', array('class' => 'form-control-label'))!!}
-                {!!Form::text('email',$user->email, ['placeholder' => '', 'class' => 'form-control col-lg-12', 'required' => '' ])!!}
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email"  value="{{ $user->email }}" class="form-control col-lg-12"/>
         </div>
-        <div class="form-group col-lg-12">
-                {!!Form::label('password', 'Password', array('class' => 'form-control-label'))!!}
-                {!!Form::text('password','', ['placeholder' => '', 'class' => 'form-control col-lg-12', 'required' => '' ])!!}
+        <div class="form-group">
+            <label for="contactno">Contact No.</label>
+            <input type="number" name="contactno" value="{{ $user->contactno }}"class="form-control col-lg-12">
         </div>
-        <div class="form-group col-lg-12">
-                {!!Form::label('password_confirmation', 'Confirm', array('class' => 'form-control-label'))!!}
-                {!!Form::text('password_confirmation','', ['placeholder' => '', 'class' => 'form-control col-lg-12', 'required' => '' ])!!}
+        <div class="form-group">
+            <label for="contactno">Password</label>
+            <input type="password" name="password" class="form-control col-lg-12"/>
+        </div>  
+        <div class="form-group">
+            <label for="contactno">Confirm Password</label>
+            <input type="password" name="password_confirmation" class="form-control col-lg-12"/>
         </div>
-        {!!Form::submit('Submit', ['id' => 'addForm','class' => 'btn btn-success mr-2']) !!}
+        <button type="submit" class="btn btn-success mr-2">Submit</button>
         <button class="btn btn-light">Cancel</button>
-    </div>
+    </form>
+    <br>
 </div>
-{!! Form::close() !!}
     @endsection
