d31b0fc284a1a8b71f50745d5522eec29cdc3969
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-10-24 12:37:29 +0800

    push

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,56 @@
 @extends('admin.master.template')
 
 @section('content')
+
+ <?php
+ 
+
+ //Best practice is to create a separate file for handling connection to database
+ try{
+     
+     $link = new \PDO(   'mysql:host=127.0.0.1;dbname=nuat_thai;charset=utf8mb4', 
+                        'root', 
+                        '', 
+                        array(
+                            \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                            \PDO::ATTR_PERSISTENT => false
+                        )
+                    );
+     
+     $handle = $link->prepare('SELECT count(*) as TOTALRES FROM bookmassages'); 
+     $handle1 = $link->prepare('SELECT count(*) as USER FROM users'); 
+     $handle2 = $link->prepare('SELECT count(*) as STAFF FROM staffs'); 
+
+
+     $handle->execute(); 
+     $result = $handle->fetchAll(\PDO::FETCH_OBJ);
+
+     $handle1->execute(); 
+     $result1 = $handle1->fetchAll(\PDO::FETCH_OBJ);
+
+     $handle2->execute(); 
+     $result2 = $handle2->fetchAll(\PDO::FETCH_OBJ);
+  
+     foreach($result as $row){
+         array($row->TOTALRES);
+     }
+     foreach($result1 as $row1){
+      array($row1->USER);
+    } 
+    foreach($result2 as $row2){
+      array($row2->STAFF);
+    } 
+
+   
+     $link = null;
+ }
+ catch(\PDOException $ex){
+     print($ex->getMessage());
+ }
+
+ ?>
+
+
 <div class="main-panel">
         <div class="content-wrapper">
           <div class="row">
@@ -34,7 +84,7 @@
                     <div class="float-right">
                       <p class="mb-0 text-right">Registered Members</p>
                       <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0">55</h3>
+                        <h3 class="font-weight-medium text-right mb-0">{{ $row1->USER}} </h3>
                       </div>
                     </div>
                   </div>
@@ -49,17 +99,17 @@
                 <div class="card-body">
                   <div class="clearfix">
                     <div class="float-left">
-                      <i class="mdi mdi-poll-box text-success icon-lg"></i>
+                    <i class="mdi mdi-account-location text-info icon-lg"></i>
                     </div>
                     <div class="float-right">
                       <p class="mb-0 text-right"></p>
                       <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0"></h3>
+                        <h3 class="font-weight-medium text-right mb-0">{{ $row2->STAFF}} </h3>
                       </div>
                     </div>
                   </div>
                   <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-calendar mr-1" aria-hidden="true"></i> Weekly Sales
+                    <i class="mdi mdi-calendar mr-1" aria-hidden="true"></i> Staffs
                   </p>
                 </div>
               </div>
@@ -69,12 +119,12 @@
                 <div class="card-body">
                   <div class="clearfix">
                     <div class="float-left">
-                      <i class="mdi mdi-account-location text-info icon-lg"></i>
+                      <i class="mdi mdi-poll-box text-success icon-lg"></i>
                     </div>
                     <div class="float-right">
                       <p class="mb-0 text-right">Reservations</p>
                       <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0">246</h3>
+                        <h3 class="font-weight-medium text-right mb-0">{{ $row->TOTALRES }} </h3>
                       </div>
                     </div>
                   </div>
@@ -176,3 +226,4 @@
                 </div>
               </div>
 @endsection
+
